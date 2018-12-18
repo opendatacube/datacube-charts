e653aec13c59a4deb7b453bf27b55c5b290cfe02
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-18 01:17:26 +0000

    Deploy opendatacube/datacube-charts to github.com/opendatacube/datacube-charts.git:gh-pages

--- deploy-charts.sh
+++ deploy-charts.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # Deploy Charts
 #
@@ -9,20 +8,29 @@ set -e
 # Info
 # Packages every chart in the folder as a tarball
 # Tries to upload each chart to the repo
+# Will perform actions twice to catch dependent charts
 
 repo="$1"
 folder="$2"
+mkdir -p charts
+n=0
+until [[ $n -gt 1 ]]; do
+    # build helm charts and dependencies
+    helm repo add datacube-charts $repo
+    helm repo update
+    # Package charts, may fail first time around
+    for chart in $folder/*
+    do
+        helm package -u $chart || :
+    done
 
-# build helm charts and dependencies
-helm repo add datacube-charts $repo
-helm repo update
-helm package -u $folder/* 
+    # copy charts that don't already exist (ignore fail messages if they do)
+    cp -nv *.tgz charts/ 2>/dev/null || :
 
-# copy charts that don't already exist (ignore fail messages if they do)
-cp -nv *.tgz charts/ 2>/dev/null || :
+    # clean up duplicate charts so they don't polute the website
+    rm *.tgz  || :
 
-# clean up duplicate charts so they don't polute the website
-rm *.tgz
-
-# rebuild index
-helm repo index charts --url $repo
+    # rebuild index
+    helm repo index charts --url $repo
+    ((n++))
+done

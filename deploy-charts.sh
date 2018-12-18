ec07ab83bb763b16c0a6efbb6248fba73e7f3d4b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-18 04:30:50 +0000

    Deploy opendatacube/datacube-charts to github.com/opendatacube/datacube-charts.git:gh-pages

--- deploy-charts.sh
+++ deploy-charts.sh
@@ -7,30 +7,39 @@
 #
 # Info
 # Packages every chart in the folder as a tarball
-# Tries to upload each chart to the repo
-# Will perform actions twice to catch dependent charts
+# Will do a single pass for requirements which exist locally
 
 repo="$1"
 folder="$2"
-mkdir -p charts
-n=0
-until [[ $n -gt 1 ]]; do
-    # build helm charts and dependencies
-    helm repo add datacube-charts $repo
-    helm repo update
-    # Package charts, may fail first time around
-    for chart in $folder/*
-    do
-        helm package -u $chart || :
-    done
 
-    # copy charts that don't already exist (ignore fail messages if they do)
-    cp -nv *.tgz charts/ 2>/dev/null || :
+# build helm charts and dependencies
+helm repo add datacube-charts $repo
+helm repo update
 
-    # clean up duplicate charts so they don't polute the website
-    rm *.tgz  || :
+# Find dependencies in datacube-charts which match $REPO
+for chart in $folder/*
+do
+    mkdir -p "${chart}/charts"
 
-    # rebuild index
-    helm repo index charts --url $repo
-    ((n++))
+    # Create local charts, do before remote to allow use of
+    # freshly updated local charts
+    helm dependency list $chart \
+    | awk -v r="$repo" 'NR>1 {if (($3 == r) && ($4 != "ok")) print $1;}' \
+    | xargs -n1 -I'{}' helm package "${folder}/{}" -d "${chart}/charts"
+    
+    #Manual fetch of remote charts
+    helm dependency list "$chart" \
+    | awk -v r="$repo" 'NR>1 {if (($3 != r) && ($4 != "ok")) print $1;}' \
+    | xargs -n1 -I'{}' helm fetch --repo "$repo" --destination "${chart}/charts" "{}"
+
+    helm package "$chart"
 done
+
+# copy charts that don't already exist (ignore fail messages if they do)
+cp -nv *.tgz charts/ 2>/dev/null || :
+
+# clean up duplicate charts so they don't polute the website
+rm *.tgz
+
+# rebuild index
+helm repo index charts --url $repo

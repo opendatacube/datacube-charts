4c783f915a2bb53ac3e7b06a6bf05f13187b03da
Damien Ayers <damien@omad.net>
2026-07-02 12:01:17 +1000

    [ci] Fix kubeval.sh some more (not sure it's worth it)

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -2,9 +2,20 @@
 set -exuo pipefail
 IFS=$'\n\t'
 
-CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-KUBEVAL_VERSION="0.16.1"
+CHART_DIRS="$(
+  git diff --find-renames --name-only remotes/origin/master -- stable |
+    awk 'tolower($0) ~ /\/chart\.yaml$/ { sub(/[\/][Cc]hart\.yaml$/, ""); print }'
+)"
+# Lowest supported k8s version as of 2026-07-02
+KUBERNETES_VERSION="1.33"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
+
+if [[ -z "${CHART_DIRS}" ]]; then
+  echo "No charts changed."
+  exit 0
+else
+  echo "The following charts have changed: ${CHART_DIRS}"
+fi
 
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/latest/download/kubeval-linux-amd64.tar.gz
